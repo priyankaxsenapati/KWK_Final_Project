67011571b3ec8385163b7f20cc84737cc96943e8
Priyanka Senapati <priyanka.usa00@gmail.com>
2021-07-21 14:40:34 -0600

    added the random array to the button with images and text

--- Final_Project/LearnAboutViewController.swift
+++ Final_Project/LearnAboutViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class LearnAboutViewController: UIViewController {
 
+    @IBOutlet weak var information: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func learnMore(_ sender: Any) {
+        img.image = [#imageLiteral(resourceName: "pngaaa.com-3462210.png"),#imageLiteral(resourceName: "tumbleweed"),#imageLiteral(resourceName: "pngaaa.com-3462210.png"),#imageLiteral(resourceName: "tumbleweed"),#imageLiteral(resourceName: "pngaaa.com-3462210.png")][Int.random(in: 0...4)]
+        information.text=["hey","hello","woohoo","hello","trial"][Int.random(in: 0...4)]
+    }
+    
     /*
     // MARK: - Navigation
 
