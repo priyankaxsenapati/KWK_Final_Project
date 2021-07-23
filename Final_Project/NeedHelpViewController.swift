fe426a33ed6c5fa6e38ac86ee5ba77332b1e3177
Priyanka Senapati <priyanka.usa00@gmail.com>
2021-07-23 11:28:29 -0600

    changed the map!

--- Final_Project/NeedHelpViewController.swift
+++ Final_Project/NeedHelpViewController.swift
@@ -11,11 +11,50 @@ class NeedHelpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        textBox.isHidden = true
 
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var textBox: UILabel!
     
+    @IBAction func atlas(_ sender: Any) {
+        textBox.text = """
+             Atlas Wood UMC
+             Hours of Operation: T-Th
+             10am-12pm/1:30-3:30pm
+             Address: 109 Alta Woods Blvd Jackson
+             MS 39204
+             Phone: 601-372-8016
+        """
+        textBox.isHidden = false
+    }
+    
+    
+    @IBAction func gateway(_ sender: Any) {
+        textBox.text = """
+             Gateway Rescue Mission
+             Hours of Operation: Sun-Sat
+             5am-5pm
+             Address: 328 S Gallatin St Jackson
+             MS 39207
+             Phone: 601-353-5864
 
+        """
+        textBox.isHidden = false
+    }
+    
+    @IBAction func keeper(_ sender: Any) {
+        textBox.text = """
+             My Brother’s Keeper
+             Hours of Operation: W&F
+             10am-1pm
+             Address: 805 River Place Dr Jackson
+             MS 39202
+             Phone: 769-216-2455
+
+        """
+        textBox.isHidden = false
+    }
     /*
     // MARK: - Navigation
 
