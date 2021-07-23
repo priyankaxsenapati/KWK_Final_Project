275709d9c3a6897a3ba7bcd978ca576ba915706d
Priyanka Senapati <priyanka.usa00@gmail.com>
2021-07-23 10:32:09 -0600

    linkss!!!

--- Final_Project/CanHelpViewController.swift
+++ Final_Project/CanHelpViewController.swift
@@ -16,7 +16,32 @@ class CanHelpViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func foodTrust(_ sender: Any) {
+        guard let url = URL(string: "https://thefoodtrust.salsalabs.org/donate/index.html") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func foodEmpower(_ sender: Any) {
+        guard let url = URL(string: "https://food-empowerment-project.networkforgood.com/projects/11476-food-empowerment-project-f-e-p") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    
+    @IBAction func wholesome(_ sender: Any) {
+        guard let url = URL(string: "https://donate.wholesomewave.org/give/306021/#!/donation/checkout") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func foodFind(_ sender: Any) {
+        guard let url = URL(string: "https://app.mobilecause.com/form/kEEGVg?vid=kcr59") else { return }
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func vid(_ sender: Any) {
+        guard let url = URL(string: "https://www.youtube.com/watch?v=v24wT16OU2w&t=295s") else { return }
+        UIApplication.shared.open(url)
+        
+    }
     /*
     // MARK: - Navigation
 
